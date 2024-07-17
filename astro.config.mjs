429c0d08951aa657c7c95095a008889c6bb9d50d
kiraaziz <kiraaziz2020@gmail.com>
2024-07-17 02:36:02 +0200

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,36 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel/serverless';
 import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
+
+// Generate the static list of dynamic URLs
+const generateDynamicUrls = () => {
+  const urls = [];
+  for (let id = 0; id <= 32; id++) {
+    urls.push(`/project/${id}`);
+  }
+  return urls;
+};
+
+const dynamicUrls = generateDynamicUrls();
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kiraaziz.vercel.app/',
-  integrations: [tailwind(), sitemap(), react()],
+  integrations: [
+    tailwind(),
+    sitemap({
+      customPages: dynamicUrls,
+    }),
+    react(),
+  ],
   output: 'server',
   adapter: vercel({
     webAnalytics: {
-      enabled: true
+      enabled: true,
     },
     speedInsights: {
-      enabled: true
-    }
-  })
+      enabled: true,
+    },
+  }),
 });
