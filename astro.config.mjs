df54f35e9d3eb2e145cb42f20c03d9db37317bd5
kiraaziz <kiraaziz2020@gmail.com>
2024-07-22 18:52:41 +0200

    sentry

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel/serverless';
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
+import sentry from "@sentry/astro";
 
 // Generate the static list of dynamic URLs
 const generateDynamicUrls = () => {
@@ -24,6 +25,13 @@ export default defineConfig({
       customPages: dynamicUrls,
     }),
     react(),
+    sentry({
+      dsn: "https://afc04dbf255d37b0840183c89396dfee@o4507629142278144.ingest.de.sentry.io/4507646489133136",
+      sourceMapsUploadOptions: {
+        project: "javascript-astro",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    }),
   ],
   output: 'server',
   adapter: vercel({
