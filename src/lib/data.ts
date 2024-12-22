0327cd303149b06a66c0bac3534401a212ce39a9
kiraaziz <kiraaziz2020@gmail.com>
2024-12-22 21:46:27 +0100

    Lay 2

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,7 +1,10 @@
 import project from "@/lib/json/project.json";
+import epicProject from "@/lib/json/project-epic.json";
 
 export const projects = [...project].sort((a: any, b: any) => {
     const priorityA = a.priority !== undefined ? a.priority : Infinity;
     const priorityB = b.priority !== undefined ? b.priority : Infinity;
     return priorityA - priorityB;
 });
+
+export const epicProjects = epicProject
