b645d063918abc53e523d18419c2baef9be368eb
kiraaziz <kiraaziz2020@gmail.com>
2025-11-22 09:02:36 +0100

    kira portofolio v3.0

--- src/components/Global/Navbar.tsx
+++ src/components/Global/Navbar.tsx
@@ -2,18 +2,19 @@
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
 import { Menu, SquareTerminal } from 'lucide-react'
 import { Button } from '@/components/ui/button'
-import { usePathname } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import { cn } from '@/utils/global'
 import { metaData } from '@/utils/content/metaData'
 import Link from 'next/link'
 import { useState } from 'react'
 
 export default function SideBar() {
-    
+
     const pathname = usePathname()
+    const router = useRouter()
 
     const [sheetOpen, setSheetOpen] = useState(false)
-    const navbar = metaData?.global?.navbar 
+    const navbar = metaData?.global?.navbar
     const { paths, title } = navbar
 
     return (
@@ -44,7 +45,7 @@ export default function SideBar() {
                     ))}
                 </div>
                 <div className="flex-1"></div>
-                <Sheet onOpenChange={(e)=>setSheetOpen(e)} open={sheetOpen}>
+                <Sheet onOpenChange={(e) => setSheetOpen(e)} open={sheetOpen}>
                     <SheetTrigger asChild>
                         <Button variant="outline" size="icon" className="flex lg:hidden bg-transparent border-0">
                             <Menu size={19} />
@@ -52,17 +53,19 @@ export default function SideBar() {
                     </SheetTrigger>
                     <SheetContent side="right" className="p-4 flex flex-col gap-2">
                         {paths.map((p) => (
-                            <Link
+                            <button
                                 key={p.to}
-                                href={p.to} 
-                                onClick={()=>setSheetOpen(false)}
+                                onClick={() => {
+                                    setSheetOpen(false)
+                                    router.push(p.to)
+                                }}
                                 className={cn(
                                     pathname === p.to ? 'font-medium' : 'lg:hover:text-foreground text-foreground/60',
                                     'ease-in-out duration-200 text-lg group'
                                 )}
                             >
                                 {p.name}
-                            </Link>
+                            </button>
                         ))}
                     </SheetContent>
                 </Sheet>
